5c434d7b1242719bee56486c8d6278aa0f363f8e
DiamondAngel1 <tararukolga78@gmail.com>
2024-12-29 20:07:52 +0200

    first commit

---
+++ CircularsQueues/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularsQueues{
+    internal class Program{
+        static void Main(string[] args){
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            CircularQueue<string> queue = new CircularQueue<string>(7);
+            queue.AddElement("1");
+            queue.AddElement("2");
+            queue.AddElement("3");
+            queue.Print();
+            Console.WriteLine("Перший елемент: " + queue.First());
+
+            Console.WriteLine("Кількість елементів: " + queue.Count());
+            Console.WriteLine("Видалено: " + queue.DelFirst());
+            Console.WriteLine("Видалено: " + queue.DelFirst());
+
+            queue.Print();
+
+            queue.AddElement("4");
+            queue.AddElement("5");
+            queue.AddElement("6");
+            queue.AddElement("7");
+            queue.AddElement("8");
+            queue.AddElement("9");
+            queue.Print();
+            Console.WriteLine("Перший елемент: " + queue.First());
+            queue.AddElement("A");
+            queue.AddElement("B");
+            queue.Print();
+
+            Console.WriteLine("Видалено: " + queue.DelFirst());
+            Console.WriteLine("Видалено: " + queue.DelFirst());
+            
+            queue.Print();
+            Console.WriteLine("Перший елемент: " + queue.First());
+        }
+    }
+}
---
+++ CircularsQueues/QueueCircular.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CircularsQueues{
+    public class CircularQueue<T>{
+        private T[] queue;
+        private int head;
+        private int tail;
+        private int count;
+        private int capacity;
+        public CircularQueue(int capacity){
+            if (capacity <= 0){
+                throw new ArgumentException("Розмір повинен бути більше нуля");
+            }
+            this.capacity = capacity;
+            queue = new T[capacity];
+            head = 0;
+            tail = 0;
+            count = 0;
+        }
+        public void AddElement(T item){
+            if (Full())
+            {
+                head = (head + 1) % capacity;
+                count--;
+            }
+            queue[tail] = item;
+            tail = (tail + 1) % capacity;
+            count++;
+        }
+        public T DelFirst(){
+            if (Empty()){
+                throw new InvalidOperationException("Черга пуста");
+            }
+            T item = queue[head];
+            head = (head + 1) % capacity;
+            count--;
+            return item;
+        }
+        public T First(){
+            if (Empty()){
+                throw new InvalidOperationException("Черга пуста");
+            }
+            return queue[head];
+        }
+        private bool Empty(){
+            return count == 0;
+        }
+        private bool Full(){
+            return count == capacity;
+        }
+        public int Count(){
+            return count;
+        }
+        public void Print(){
+            Console.Write("Черга: ");
+            for (int i = 0; i < count; i++){
+                int index = (head + i) % capacity;
+                Console.Write(queue[index] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
